9ecdc91dd38d15eb6dc28e0d641fa75913d0a9a6
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2021-07-07 15:25:52 -0300

    Detalhe final
    Tinha errado o uso da variável e package XD

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
@@ -23,32 +23,32 @@ func main() {
 	reg = os.Getenv("AWS_REGION")
 	aki = os.Getenv("AWS_ACCESS_KEY_ID")
 	sak = os.Getenv("AWS_SECRET_ACCESS_KEY")
-	st  = os.Getenv("AWS_SESSION_TOKEN")
-	pn  = os.Getenv("AWS_PARAMETER_NAME")
+	st = os.Getenv("AWS_SESSION_TOKEN")
+	pn = os.Getenv("AWS_PARAMETER_NAME")
 
 	// Creating a new AWS Config
 	c := &aws.Config{
 		Credentials: credentials.NewStaticCredentials(aki, sak, st),
-		Region: aws.String(reg),
+		Region:      aws.String(reg),
 	}
 
 	// Creating a new AWS Session
 	sess := session.Must(session.NewSession())
 
 	// Creating a new AWS SSM Service Client
-	ssm := ssm.New(sess, c)
+	sc := ssm.New(sess, c)
 
 	// Reading a Parameter from AWS Parameter Store:
 	// First, we create a GetParameterInput
-	pi := ssm.GetParameterInput{
+	pi := &ssm.GetParameterInput{
 		// The Parameter Name
 		Name: aws.String(pn),
 		// To get the Parameter value decrypted, when using SecureString
 		WithDecryption: aws.Bool(true),
 	}
 	// Then, we request GetParameter
-	out, err := ssm.GetParameter(pi)
-	if (err != nil) {
+	out, err := sc.GetParameter(pi)
+	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
